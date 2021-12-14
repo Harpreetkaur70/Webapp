dcb39174675201d162eebc415f1a24bf3592730b
Harpreetkaur70 <90725247+Harpreetkaur70@users.noreply.github.com>
2021-12-14 20:30:53 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Webapp.Models;
+
+namespace Webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult Index(DateTime date,int days)
+        {
+            
+            ViewBag.date = string.Format("Date: {0}",date);
+            ViewBag.days = string.Format("Days: {0}",days);
+
+           ViewBag.dat = "Result: "+date.AddDays(days);
+         
+            return View();
+        }
+    }
+}
